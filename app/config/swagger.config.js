24dc87ba2ac9c82b8e203f969776c57b83c10791
Indrani Mukhopadhyay <indranimukhopadhyay17@gmail.com>
2026-01-22 13:14:42 +0530

    Initial commit

--- app/config/swagger.config.js
+++ app/config/swagger.config.js
@@ -4,21 +4,64 @@ module.exports = {
     info: {
       title: "Product API",
       version: "1.0.0",
+      description: "Product API documentation",
+      contact: {
+        name: "indrani",
+      },
     },
-    servers: [{ url: "http://localhost:5000" }],
+
+    servers: [
+      {
+        url: "http://localhost:5000",
+        description: "Local HTTP server",
+      },
+      {
+        url: "https://localhost:5000",
+        description: "Local HTTPS server",
+      },
+      {
+        url: "https://your-render-app.onrender.com",
+        description: "Production HTTPS server",
+      },
+    ],
+
+    // tags: [
+    //   {
+    //     name: "restful API",
+    //     description: "CRUD API",
+    //   },
+    //   {
+    //     name: "Auth restful API",
+    //     description: "Auth API",
+    //   },
+    //   {
+    //     name: "Blog API",
+    //     description: "Blog API",
+    //   },
+    // ],
+
     components: {
       securitySchemes: {
         bearerAuth: {
           type: "http",
           scheme: "bearer",
           bearerFormat: "JWT",
+          description: "Bearer token needed to access this API",
         },
       },
     },
+
+    security: [
+      {
+        bearerAuth: [],
+      },
+    ],
   },
-  apis: 
-    ["./app/routes/auth.routes.js",
-   "./app/routes/product.routes.js"]
-  
-  
+
+  apis: [
+    "app.js",
+    "./app/routes/auth.routes.js",
+    "./app/routes/product.routes.js",
+    "./app/routes/*.js",
+  ],
 };
